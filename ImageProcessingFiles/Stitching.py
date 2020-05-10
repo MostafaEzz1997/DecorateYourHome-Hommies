5154663178e1f24bb33ba4d1ef066491288f3f5d
Ahmed Ashraf Mahmoud <31229408+a1h2med@users.noreply.github.com>
2020-05-10 16:59:59 +0200

    Stitching_Code
    This Code Aims to take multiple images and reconstruct a full view image.

--- ImageProcessingFiles/Stitching.py
+++ ImageProcessingFiles/Stitching.py
@@ -1,8 +1,10 @@
 import cv2
 import numpy as np
 
-dim = (1024, 768) # new dimenstions
+dim = (1024, 768) # new dimensions
 
+# we are resizing images to make sure that they are all the same size, also to make it easier
+# for the stitching function to work.
 left = cv2.imread('left0.jpg', cv2.IMREAD_COLOR) #read first image
 
 left = cv2.resize(left, dim, interpolation=cv2.INTER_AREA) # ReSize to (1024,768)
@@ -11,6 +13,7 @@ right = cv2.imread('right0.jpg', cv2.IMREAD_COLOR)#read second image
 
 right = cv2.resize(right, dim, interpolation=cv2.INTER_AREA)# ReSize to (1024,768)
 
+
 images = []      #list of images
 
 images.append(left)    #add the first image to the list
@@ -18,11 +21,10 @@ images.append(left)    #add the first image to the list
 images.append(right)   #add the second image to the list
 
 stitcher = cv2.Stitcher.create()
-ret, pano = stitcher.stitch(images) #return two values first value panorama of the two images and 1 or pano is empty and 0
-#if ret =1  show pano 
+ret, pano = stitcher.stitch(images) #return two values first value (panorama of the two images and 1) or pano is (empty and 0)
+# if ret == 1  show pano 
 if ret == cv2.STITCHER_OK:
     cv2.imshow('Panorama', pano)
     cv2.waitKey()
-    cv2.destroyAllWindows()
 else:
     print("ERROR")
